e6bc79057718beca33a33cb4f929db56b6813963
Ivista <bevanc6@gmail.com>
2021-12-04 16:16:39 +0000

    Added asg subnets and launch config

--- main.tf
+++ main.tf
@@ -14,12 +14,42 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = "ami-830c94e3"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-asg-example"
+  vpc_zone_identifier  = ["subnet-6e6a0b34", "subnet-3bb33e42"]
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 2
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
